57850da7f93eddd0ca5c730c908b377bf15d82d8
Lulseged Admasu <lulsegedwork@gmail.comu>
2022-08-27 22:44:53 -0400

    Styling for biggesst screen

--- assets/js/active.js
+++ assets/js/active.js
@@ -4,89 +4,113 @@ const header_logo = document.querySelector(".header-logo-w");
 const secondary_header = document.querySelector(".secondary-header");
 const nav_txts = document.querySelectorAll('.nav-txt');
 const nav_contact_us = document.querySelectorAll('header nav .btn');
-// const blog = document.querySelector('header nav .btn-0');
+const home = document.querySelector('.hme');
+const blog = document.querySelector('header nav .btn-0');
+const to_top_btn = document.querySelector('.to-top-btn');
 
 
 const not_headers = document.querySelectorAll('.not-header');
 const nav_toggle = document.querySelector(".toggle-nav");
 const nav_toggle_i = nav_toggle.getElementsByTagName("i")[0];
 const plane = document.querySelector(".plane");
+const blog_icon = document.querySelector(".blog");
 
-// nav toggle outside of header click
-not_headers.forEach((not_header) => {
-    not_header.addEventListener('click', () => {
-        const is_visible = nav.getAttribute("data-visible");    
-        if(is_visible === "true") {
+
+// remove/add nav
+function remove_nav() {
             nav.setAttribute("data-visible", false);
+            // nav.style.background = 'transparent';
             nav_toggle.setAttribute("data-x", false);
             nav_toggle_i.classList.add("fa-bars");
             nav_toggle_i.classList.remove("fa-x");
             nav_toggle.style.position = "absolute";
             plane.style.position = "absolute";
-        }
- 
-    })
-});
+            blog_icon.style.position = "absolute";
 
-// nav toggle button click
-nav_toggle.addEventListener('click', () => {
-    const is_visible = nav.getAttribute("data-visible");    
-    //if nav not visible
-    if(is_visible === "false" || nav_toggle.style.position === "absolute") {
+}
+function add_nav() {
+        // nav.style.background = 'black';
         nav.setAttribute("data-visible", true);
         nav_toggle.setAttribute("data-x", true);
         nav_toggle_i.classList.remove("fa-bars");
         nav_toggle_i.classList.add("fa-x");
         nav_toggle.style.position = "fixed";
         plane.style.position = "fixed";
+        blog_icon.style.position = "fixed";
+}
+const goToTop = () => {
+    document.body.scrollIntoView({
+      behavior: "smooth"
+    });
+  };
+to_top_btn.addEventListener("click", goToTop)
+home.addEventListener("click", goToTop)
+
+// nav toggle outside of header click
+not_headers.forEach((not_header) => {
+    not_header.addEventListener('click', () => {
+        const is_visible = nav.getAttribute("data-visible");    
+        if(is_visible === "true") {
+            remove_nav();
+        }
+ 
+    })
+});
+// nav toggle button click
+nav_toggle.addEventListener('click', () => {
+    const is_visible = nav.getAttribute("data-visible");    
+    //if nav not visible
+    if(is_visible === "false" || nav_toggle.style.position === "absolute") {
+        add_nav();
     }
     else {
-        nav.setAttribute("data-visible", false);
-        nav_toggle.setAttribute("data-x", false);
-        nav_toggle_i.classList.add("fa-bars");
-        nav_toggle_i.classList.remove("fa-x");
-        nav_toggle.style.position = "absolute";
-        plane.style.position = "absolute";
+        remove_nav();
     }
 });
 
-
-// hide nav on scroll down, show on scroll up
+//scroll header-nav behavior
 var lastTopScroll = 0; 
 window.addEventListener("scroll", () => {
+console.log(window.innerWidth);
+
     const is_visible = nav.getAttribute("data-visible");    
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     if(scrollTop > lastTopScroll && scrollTop > 50) {
+        to_top_btn.style.right = '5vh';
         header.style.top = '-15vh';
         header.style.background="white";
         nav_toggle.style.color="black";
         plane.style.color="black";
+        blog_icon.style.color="black";
         header_logo.src = "assets/img/logo/logo.png";
         nav_txts.forEach((nav_txt) => {nav_txt.style.color = 'black'});
         nav.style.background = 'white';
-        nav_contact_us[0].style.borderColor = 'white';
-        nav_contact_us[1].style.borderColor = 'white';
-
-
+        nav_contact_us[0].style.borderColor = 'black';
+        nav_contact_us[1].style.borderColor = 'black';
     }
     else if (scrollTop === 0) {
         header.style.background="transparent";
         nav_toggle.style.color="white";
         plane.style.color="white";
+        blog_icon.style.color="white";
         header_logo.src = "assets/img/logo/logo-white.png";
         nav_txts.forEach((nav_txt) => {nav_txt.style.color = 'white'});
         nav.style.background = '#06052f';
         nav_contact_us[0].style.borderColor = 'white';
         nav_contact_us[1].style.borderColor = 'white';
-
+        to_top_btn.style.right = '-10vh';
     }
     else {
         header.style.top = '0';
-     
     }
     lastTopScroll = scrollTop;
 });
 
+// nav link click remove navbar 
+nav_txts.forEach((nav_txt) => {
+    nav_txt.addEventListener("click", ()=> {
+        setTimeout(() => {remove_nav()}, 200);
+    })
+});
 
-//secondary header is white - change each style on scrolltop
-//primary is transparent
+//nav bg for screen sizes
